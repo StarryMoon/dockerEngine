46f3c4cba8a5d55725eec733883baff424ea1882
StarryMoon <foggyrmail@163.com>
2018-02-01 02:11:47 +0000

    correct some mistakes

--- src/cgroups/subsystems/memory.go
+++ src/cgroups/subsystems/memory.go
@@ -16,6 +16,7 @@ func (s *MemorySubsystem) Set(cgroupPath string, res *ResourceConfig) error {
     if subsysCgroupPath, err := GetCgroupPath(s.Name(), cgroupPath, true); err == nil {
         if res.MemoryLimit != "" {
             if err := ioutil.WriteFile(path.Join(subsysCgroupPath, "memory.limit_in_bytes"), []byte(res.MemoryLimit), 0644); err != nil {
+                fmt.Println("set cgroup memory failed : ", err)
                 return fmt.Errorf("set cgroup memory fail %v", err)
             }
         }
--- src/cgroups/subsystems/utils.go
+++ src/cgroups/subsystems/utils.go
@@ -41,7 +41,8 @@ func FindCgroupMountPoint(subsystem string) string{
 //
 func GetCgroupPath(subsystem string, cgroupPath string, autoCreate bool) (string, error) { 
     cgroupRoot := FindCgroupMountPoint(subsystem)
-    if _, err := os.Stat(path.Join(cgroupRoot, cgroupPath)); err != nil || (autoCreate && os.IsNotExist(err)) {
+    fmt.Println("cgroup mount point : ", cgroupRoot)
+    if _, err := os.Stat(path.Join(cgroupRoot, cgroupPath)); err == nil || (autoCreate && os.IsNotExist(err)) {
         if os.IsNotExist(err) {
             if err := os.Mkdir(path.Join(cgroupRoot, cgroupPath), 0755); err == nil {
             
