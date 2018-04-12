855b53f4feef2ffe7b20ade7d548d16114ff53b5
StarryMoon <foggyrmail@163.com>
2018-04-12 05:46:56 +0000

    supporting the aufs & mount point

--- src/container/container_process.go
+++ src/container/container_process.go
@@ -27,7 +27,15 @@ func NewParentProcess(tty bool) (*exec.Cmd, *os.File) {
     }
     
     cmd.ExtraFiles = []*os.File{readPipe}
-    cmd.Dir = "/root/busybox"
+
+    //cmd.Dir = "/root/busybox"
+    
+    //specify the foot file system
+    mntURL := "/root/mnt/"
+    rootURL := "/root/"
+    NewWorkSpace(rootURL, mntURL)
+    cmd.Dir = mntURL
+
     return cmd, writePipe
 }
 
@@ -39,3 +47,68 @@ func NewPipe() (*os.File, *os.File, error) {
     return read, write, nil
 }
 
+func NewWorkSpace(rootURL string, mntURL string) {
+    CreateReadOnlyLayer(rootURL)
+    CreateWriteLayer(rootURL)
+    CreateMountPoint(rootURL, mntURL)
+}
+
+
+//decompress the tar package of read only file system
+func CreateReadOnlyLayer(rootURL string) {
+    busyboxURL := rootURL + "busybox/"
+    busyboxTarURL := rootURL + "busybox.tar"
+    exist, err := PathExists(busyboxURL)
+    if err != nil {
+        log.Infof("Fail to judge whether the directory %s exists. %v ", busyboxURL, err)
+    }
+    
+    if exist == false {
+        if err := os.Mkdir(busyboxURL, 0777); err != nil {
+            log.Errorf("Mkdir directory %s error.  %v", busyboxURL, err)
+        }
+
+        if _, err := exec.Command("tar", "-xvf", busyboxTarURL, "-C", busyboxURL).CombinedOutput(); err != nil {
+            log.Errorf("unTar directory %s error %v", busyboxTarURL, err)
+        }
+    }
+
+}
+
+//create a read-write layer
+func CreateWriteLayer(rootURL string) {
+    writeURL := rootURL + "writeLayer/"
+    if err := os.Mkdir(writeURL, 0777); err != nil {
+        log.Errorf("Mkdir directory %s error %v", writeURL, err)
+    }
+}
+
+//mount rw layer and ro layer
+func CreateMountPoint(rootURL string, mntURL string) {
+    //create mnt mount point
+    if err := os.Mkdir(mntURL, 0777); err != nil {
+        log.Errorf("Mkdir directory %s error %v", mntURL, err)
+    }
+
+    //"mount -t aufs -o dirs=/root/writeLayer:/root/busybox/ none /root/mnt"
+    dirs := "dirs=" + rootURL + "writeLayer:" + rootURL + "busybox"
+    cmd := exec.Command("mount", "-t", "aufs", "-o", dirs, "none", mntURL)
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+    if err := cmd.Run(); err != nil {
+        log.Errorf("%v", err)
+    }
+}
+
+func PathExists(path string) (bool, error) {
+    _, err := os.Stat(path)
+    if err == nil {
+        return true, nil
+    }
+
+    if os.IsNotExist(err) {
+        return false, nil
+    }
+
+    return false, err
+}
