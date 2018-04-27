397bdd57b43aed44fd5503f74d46057fa43f21b5
StarryMoon <foggyrmail@163.com>
2018-04-27 07:29:07 +0000

    support list container info

--- src/list.go
+++ src/list.go
@@ -27,9 +27,23 @@ func listContainer() {
         }
         containers =append(containers, tmpContainer)
     }
-   
-    
 
+    w := tabwriter.NewWriter(os.Stdout, 12, 1, 3, ' ', 0)
+    fmt.Fprint(w, "ID\tNAME\tPID\tSTATUS\tCOMMAND\tCREATED\n")
+    for _, item := range containers {
+        fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t%s\n",
+            item.Id,
+            item.Name,
+            item.Pid,
+            item.Status,
+            item.Command,
+            item.CreateTime)
+    }
+    
+    if err := w.Flush(); err != nil {
+        log.Errorf("Flush error %v", err)
+        return
+    }
 }
 
 
@@ -38,4 +52,17 @@ func getContainerInfo(file os.FileInfo) (*container.ContainerInfo, error) {
     containerName := file.Name()
     configFileDir := fmt.Sprintf(container.DefaultInfoLocation, containerName)
     configFileDir = configFileDir + container.ConfigName
+    content, err := ioutil.ReadFile(configFileDir)
+    if err != nil {
+        log.Errorf("Read file %s error %v", configFileDir, err)
+        return nil, err
+    }
+
+    var containerInfo containerInfo
+    if err := json.Unmarshal(content, &containerInfo); err != nil {
+        log.Errorf("Json unmarshal error %v", err)
+        return nil, err
+    }
+
+    retrun &containerInfo, nil
 }
