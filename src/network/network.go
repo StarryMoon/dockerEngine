3821ef726bdbc35295a613771d23df40722a595d
StarryMoon <foggyrmail@163.com>
2018-05-29 05:41:07 +0000

    add some network codes

--- src/network/network.go
+++ src/network/network.go
@@ -4,6 +4,7 @@ import (
     "github.com/vishvananda/netlink"
     "github.com/vishvananda/netns"
     "fmt"
+    "net"
     "dockerEngine/src/container"
 )
 
@@ -28,4 +29,39 @@ type NetworkDriver interface {
     Delete(network Network) error
     Connect(network *Network, endpoint *Endpoint) error
     Disconnect(network Network, endpoint *Endpoint) error
+}
+
+func CreateNetwork(driver string, subnet string, name string) error {
+    _, cidr, _ := net.ParseCIDR(subnet)
+    gatewayIP, err := ipAllocator.Allocate(cidr)
+    if err != nil {
+        return err
+    }
+    cidr.IP = gatewayIP
+    fmt.Println("network/network.go CreateNetwork()  cidr.IP : ", cidr.IP)
+    
+    nw, err := drivers[driver].Create(cidr.String(), name)
+    if err != nil {
+        return err
+    }
+
+    return nw.dump(defaultNetworkPath)
+}
+
+func (nw *Network) dump(dumpPath string) error {
+    if _, err := os.Stat(dumpPath); err != nil {
+        if os.IsNoExist(err) {
+            os.MkdirAll(dumpPath, 0644)
+        }else {
+            return err
+        }
+    }
+
+    nwPath : = path.join(dumpPath, nw.Name)
+    nwFile, err := os.OpenFile(nwPath, os.O_TRUNC | os.O_WRONLY | os.O_CREATE, 0644)
+    if err != nil {
+        logrus.Errorf("error: ", err)
+        return err
+    }
+    defer nwFile.Close()
 }
